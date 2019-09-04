7d874a9dcb60c3ea76b1b2f24a60c075d3b03b83
Elliott Esponda <e_e102@txstate.edu>
2019-09-04 18:05:02 -0500

    adds query chain

--- myApp.js
+++ myApp.js
@@ -309,8 +309,13 @@ var removeManyPeople = function(done) {
 
 var queryChain = function(done) {
   var foodToSearch = "burrito";
-  
-  done(null/*, data*/);
+  Person.find({favoriteFoods:foodToSearch})
+    .sort({name : "asc"})
+    .limit(2).select("-age")
+    .exec((err, data) => {     
+      if(err) done(err);
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
